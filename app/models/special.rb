ff21d7f22f6a7ccbafbd215551756e29cf1f7032
Isaac Falkenstine <isaac@falkenstine.com>
2018-10-07 21:14:16 -0600

    Added specials (User story 2)

---
+++ app/models/special.rb
@@ -0,0 +1,10 @@
+class Special < ActiveRecord::Base
+
+  validates_presence_of :title, :comedian_id
+
+  belongs_to :comedian
+
+  def self.average_runtime
+    average(:runtime) ? average(:runtime).floor : "N/A"
+  end
+end
